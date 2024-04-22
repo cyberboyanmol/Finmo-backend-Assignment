9e6ce2a990dde071ad8d6c00eecb845517a0c26f
Anmol Gangwar <anmolgangwar64@gmail.com>
2024-04-22 19:50:03 +0530

    feat :sparkles: added the workers app in monorepo

--- apps/cron/src/tasks/forex-rate-sync.task.ts
+++ apps/cron/src/tasks/forex-rate-sync.task.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
+
 import {
   CurrencyCode,
   currencyCodesWithName,
@@ -13,13 +15,15 @@ export class ForexSyncService {
   constructor(private readonly configService: ConfigService) {}
   @Cron(CronExpression.EVERY_MINUTE)
   async syncForexRates() {
+    const forexEchangeRatesId = 'nanoid';
+
     currencyCodesWithName.map(async (currency: CurrencyCode) => {
       const params: buildForexExchangeRateUrlProps = {
         to_currency: currency.code,
         apiKey: this.configService.ALPHA_VANTAGE_API_KEYS,
       };
       const url = buildForexExchangeRateUrl(params);
-      console.log(url);
+      console.log(url, forexEchangeRatesId);
       // ✅ TODO
       //  Added the url to the queue for processing currency exchange rate and saving to the db
     });
